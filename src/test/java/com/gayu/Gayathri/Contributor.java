a26c5abe05feb5d9e82ae222575b89fc36582bc7
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-08 13:10:17 +0530

    Contributor.java

--- src/test/java/com/gayu/Gayathri/Contributor.java
+++ src/test/java/com/gayu/Gayathri/Contributor.java
@@ -154,7 +154,7 @@ public class Contributor {
 		public void select_contributor() {
 			try {
 				WebDriverWait wait = new WebDriverWait(driver, 30);
-				WebElement radio = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='radio'])[2]")));
+				WebElement radio = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//input[@type='radio'])[3]")));
 				radio.click();
 				Thread.sleep(3000);
 				System.out.println("--------------------------*****************-----------------------");
